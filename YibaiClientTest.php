61ad0e6a432c7e964bfdb6c63c2040aa0450ae13
杨雪松 <yangxuesong@shomop.com>
2017-10-17 13:50:08 +0800

    yibai-php-sdk

---
+++ YibaiClientTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('YibaiSdk.php');
+
+$client = new YibaiClient('https://sms.100sms.cn/api', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+try {
+    $response = $client->smsBatchSubmit(array(
+        new SmsSubmit('186xxxxxxxx', '【亿佰云通讯】您的验证码是：111111'),
+        new SmsSubmit('187xxxxxxxx', '【亿佰云通讯】您的验证码是：222222')
+    ));
+    print_r($response);
+} catch (YibaiApiException $e) {
+    print_r('YibaiApiException, code: ' . $e->getCode() . ', message: '. $e->getMessage());
+} catch (Exception $e) {
+    print_r('Exception. message: ' . $e->getMessage());
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Version 1.0.0
+
+具体使用方法可参考该目录下的YibaiClientTest.php文件中的代码
